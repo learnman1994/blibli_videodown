de3aab1859ded11ef9056bbe65e1cdd7e3afbb44
cattypapa <jxsjdcat@163.com>
2021-04-28 13:14:37 +0800

    修改几处语句

--- b_main.py
+++ b_main.py
@@ -4,11 +4,14 @@ from b_down_file import b_more_part
 
 def main():
     while True:
-        print('*'*20)
+        print('*'*50)
         print('欢迎使用B站视频下载工具~')
-        print('输入exit,退出程序')
-        choice = input('单P下载，请输入1, 多P下载请输入2:')
-        print('*'*20)
+        choice_list = ['单P下载输入: 1','多P下载输入: 2', '退出输入: exit']
+        for choice in choice_list:
+            print(choice)
+        print('*'*50)
+        choice = input('请输入:')
+        print('*'*50)
         if choice == '1':
             b_single_part.main()
         elif choice == '2':
